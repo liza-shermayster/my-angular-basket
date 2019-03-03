c7d76c2534ef46b80933be692bf3fc0dd237031e
Liza Shermayster <lizashermayster@Pavels-MacBook-Pro-2.local>
2019-03-03 22:09:35 +0200

    simple basket implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { addAllToArray } from '@angular/core/src/render3/util';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   fruits = [
     { title: 'apple', price: 1 },
@@ -12,11 +14,24 @@ export class AppComponent {
     { title: 'mango', price: 5 }
   ];
   basket: string;
-  total;
+  total = 0;
+  countBasket = [];
+  totalPrice = 0;
+  amount = 0;
+  myTotalPriceItem = [
+    { tile: 'apple', price: 1, count: 2 },
+    { tile: 'banana', price: 3, count: 4 },
+    { tile: 'mango', price: 5, count: 6 }
+  ];
+  test = { title: 'apple', count: '2', price: '3' };
 
   getDataFun(event) {
-    console.log(event);
-    this.basket = event.title;
     this.total = event.price;
+    this.basket = event.title;
+    this.countBasket.push(event.title);
+    this.amount = this.amount + 1;
+    this.totalPrice = this.totalPrice + event.price;
   }
+
 }
+
