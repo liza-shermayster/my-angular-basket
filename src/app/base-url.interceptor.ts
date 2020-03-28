08d924328075a7b25c59adca7dcc4df57e6ebf87
Liza Shermayster <lizashermayster@Lizas-MacBook-Pro.local>
2020-03-28 18:02:08 +0300

    interceptor changed

--- src/app/base-url.interceptor.ts
+++ src/app/base-url.interceptor.ts
@@ -10,11 +10,11 @@ import { environment } from 'src/environments/environment';
 
 @Injectable()
 export class BaseUrlInterceptor implements HttpInterceptor {
-  baseUrl = environment.production ?  'https://web-shop-liza.herokuapp.com/api/' : 'http://localhost:3000/api/';
+  baseUrl = isDevMode() ? 'http://localhost:3000/api/' : 'https://web-shop-liza.herokuapp.com/api/';
   constructor() { }
 
   intercept(req: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    debugger;
+
     const apiReq = req.clone({ url: this.baseUrl + req.url });
     return next.handle(apiReq);
   }
